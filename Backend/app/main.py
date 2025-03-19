b257242b384a1b3d4925603adb5f411744edbd9b
Jatin Gupta <jatingupta108@gmail.com>
2025-03-19 22:12:39 +0530

    changed unicorn.run

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -82,6 +82,6 @@ async def websocket_endpoint(websocket: WebSocket):
 
 
 if __name__ == "__main__":
-    # uvicorn.run(app , host = '0.0.0.0')
-    uvicorn.run(app , host = 'localhost' , port =8000)
+    uvicorn.run(app , host = '0.0.0.0')
+    # uvicorn.run(app , host = 'localhost' , port =8000)
 
